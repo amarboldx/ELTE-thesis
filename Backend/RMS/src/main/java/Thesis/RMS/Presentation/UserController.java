ae3bbf5100aa2bcef696b9a75c577d2352be38d7
amarboldx <amarboldtuv08@gmail.com>
2025-03-15 15:44:29 +0100

    Rebase project structure: move backend Spring project files to root

--- Backend/RMS/src/main/java/Thesis/RMS/Presentation/UserController.java
+++ Backend/RMS/src/main/java/Thesis/RMS/Presentation/UserController.java
@@ -25,9 +25,7 @@ import java.util.stream.Collectors;
 public class UserController {
 
     private final UserService userService;
-    private final UserDetailsService userDetailsService;
     private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
     private final JwtUtils jwtUtils;
 
     @PostMapping("/register")
